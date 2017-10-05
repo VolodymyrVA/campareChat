264fcc887ad655e31001823aaae53c7b00ecd75e
Volodymyr Vysotskyi (Kharkiv) <v.vysotskyi@mobidev.biz>
2017-10-05 13:35:36 +0300

    full prepare app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,25 +6,33 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import {HomeModule} from "../pages/home/home.module";
+import { DataProvider } from '../providers/data/data';
+import {IonicStorageModule} from "@ionic/storage";
+import {InAppBrowser} from "@ionic-native/in-app-browser";
+import {Keyboard} from "@ionic-native/keyboard";
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    IonicStorageModule.forRoot(),
+    HomeModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    MyApp
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    DataProvider,
+    InAppBrowser,
+    Keyboard
   ]
 })
 export class AppModule {}
